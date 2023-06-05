1e97551a692bc5dc188b26b29639ec306f6c529f
Clement Olivier <clement.olivier@eiffage.com>
2023-06-05 11:03:47 +0200

    deconnexion fonctionnelle

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth-service';
 import { ProfileService } from 'src/app/services/profile-service';
 
 @Component({
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit{
   searchTerm ='';
 
   constructor(activatedRoute:ActivatedRoute,private router:Router,
-   private profileService:ProfileService){
+   private profileService:ProfileService, private auth: AuthService){
     activatedRoute.params.subscribe((params)=>{
       if(params['searchTerm'])
       this.searchTerm = params['searchTerm'];
@@ -26,6 +27,11 @@ export class HeaderComponent implements OnInit{
   search(term:string):void{
     if(term)
     this.router.navigateByUrl('/search/'+term);
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigateByUrl('');
   }
 
 }
